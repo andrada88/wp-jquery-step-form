6026c924ee544328bc853dd966b890021da3d2f8
Andrada Crisan <andradacrisan@gmail.com>
2015-11-04 00:51:53 +0200

    Finally working

--- wp-jquery-step-form.php
+++ wp-jquery-step-form.php
@@ -1,7 +1,13 @@
 <?php
 
-/*
-first test
+/**
+ * Plugin Name: Wordpress jQuery Step Form
+ * Plugin URI: http://www.creativeaura.ro
+ * Description: This plugin adds jQuery step form to a page.
+ * Version: 1.0.0
+ * Author: Adriana Crisan
+ * Author URI: http://www.creativeaura.ro
+ * License: GPL2
  */
 
 
